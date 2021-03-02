45fac5e550aff70388f7a02f30f3c7feb0b180dc
torhen <torhen@gmail.com>
2021-03-03 00:22:29 +0100

    better

--- psg_typing_trainer.py
+++ psg_typing_trainer.py
@@ -18,11 +18,17 @@ def get_text(url, line_length):
 
 def main():
     line_length = 100
-    layout = [[sg.Input(key='-URL-', size=(line_length,),default_text='http://www.spiegel.de'), sg.Button('get', key='-GET-')],
-              [sg.Multiline(size=(line_length, 40), key='-TXT-', font=('', 14))]
+    font_size = 14
+    txtfield_size = (100, 30)
+
+    layout = [[sg.Input(key='-URL-', size=(100,),default_text='http://www.spiegel.de'), sg.Button('get', key='-GET-')],
+              [sg.Multiline(size=txtfield_size, key='-TXT-', font=('', font_size), disabled=True)]
               ]
 
-    win = sg.Window('type trainer', layout)
+    win = sg.Window('type trainer', layout, return_keyboard_events=True, use_default_focus=False)
+
+    txt_field = win['-TXT-']  # type:sg.Multiline
+    txt_content = ''
 
     while True:
         event, values = win.read()
@@ -31,11 +37,24 @@ def main():
         elif event == '-GET-':
             url = values['-URL-']
             lines = get_text(url, line_length)
-            ml = win['-TXT-'] # type:sg.Multiline
-            ml.update('')
-            for line in lines:
-                ml.print(line, text_color='blue')
-                ml.print('', text_color='black')
+            txt_content = '\n'.join(lines)
+            #txt_content = "Das ist ein Test\nein weiterer Test"
+            txt_pos = 0
+            update_text(txt_field, txt_content, txt_pos, font_size=font_size)
+        elif len(event) == 1:   # key strokes
+            print(event)
+            if txt_pos < len(txt_content) - 1:
+                txt_pos += 1
+            update_text(txt_field, txt_content, txt_pos, font_size=font_size)
+
+
+def update_text(txt_field, txt_content, txt_pos, font_size):
+    txt_field.update('')
+    txt_field.print(txt_content[:txt_pos], end='', text_color='black')
+    txt_field.print(txt_content[txt_pos], end='', text_color='white', background_color='#00AA00')
+    txt_field.print(txt_content[txt_pos+1:], end='', text_color='black')
+    txt_field.set_focus()
+    txt_field.set_vscroll_position(0)
 
 
 if __name__ == '__main__':
