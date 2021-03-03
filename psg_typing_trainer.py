328883f7f1e8ace948ca99a8e8cbabac179dd994
torhen <torhen@gmail.com>
2021-03-03 17:09:43 +0100

    cursor move

--- psg_typing_trainer.py
+++ psg_typing_trainer.py
@@ -6,19 +6,17 @@ import textwrap
 
 class Content:
 
-    def __init__(self, canvas_size):
+    def __init__(self, graph, canvas_size):
+        self.graph = graph # type:sg.graph
         self.lines = []
-        self.pos = 0
         self.font = ('Courier', 14)
-        self.line_spacing = 30
+        self.line_spacing = 33
         self.char_spacing = 11
         self.canvas_size = canvas_size
-        self.show_lines = int(1.5 * canvas_size[1] / self.line_spacing)
         self.green = "#00AA00"
-        self.cursor_pos = 0
+        self.cursor_line = 0
+        self.cursor_char = 0
 
-    def get_text(self):
-        return '\n'.join(self.lines)
 
     def from_url(self, url, line_length):
         r = requests.get(url)
@@ -32,54 +30,47 @@ class Content:
         self.lines = lines
         self.pos = 0
 
-    def change_pos(self, delta):
-        new_pos = self.pos + delta
-        if new_pos <=0:
-            new_pos = 0
-        self.pos = new_pos
-
-    def goto_line(self, nline):
-        n = self.get_current_line()
+    def print_text(self):
         pos = 0
-        for i in range(nline):
-            pos = pos + len(self.lines[i]) + 1
-        self.pos = pos
-
-    def print_text(self, text_field:sg.Graph, line_nmb):
-        pos = 0
-        text_field.draw_rectangle(top_left=(0, 0), bottom_right=(30, 30), fill_color=self.green)
-        lines = self.lines[line_nmb: line_nmb + self.show_lines]
-        text_field.erase()
+        self.graph.erase()
+        lines = self.lines
         for i, line in enumerate(lines):
-            text_field.draw_text(line, location=(0, pos),
-                    text_location=sg.TEXT_LOCATION_TOP_LEFT, font = self.font)
+            self.graph.draw_text(line, location=(0, pos),
+                    text_location=sg.TEXT_LOCATION_TOP_LEFT,
+                    font = self.font)
+
             pos = pos + self.line_spacing
 
-        self.cursor_obj = text_field.draw_rectangle(top_left=(self.cursor_pos, 30),
-                                  bottom_right=(self.cursor_pos + self.char_spacing, 30),
-                                  line_color=self.green, line_width=5)
+        # Cursor
+        top_left = (self.cursor_char * self.char_spacing, self.line_spacing)
+        bottom_right = (self.char_spacing, self.line_spacing)
+
+        self.cursor_obj = self.graph.draw_rectangle(
+                                top_left=top_left,
+                                bottom_right=bottom_right,
+                                line_color=self.green,
+                                line_width=5)
+
+        self.set_cursor(0, 0)
+
+
+    def get_cursor(self):
+        return self.cursor_char, self.cursor_line
+
+    def set_cursor(self, char, line):
+        self.cursor_char = char
+        self.cursor_line = line
+
+        curs_x = self.cursor_char * self.char_spacing
+        curs_y = self.cursor_line * self.line_spacing + self.line_spacing
+        self.graph.relocate_figure(self.cursor_obj, curs_x, curs_y)
+
+    def move_cursor(self, dx, dy):
+        cur_x, cur_y = self.get_cursor()
+        self.set_cursor(cur_x + dx, cur_y + dy)
 
     def key_press(self, text_field, pressed_key):
         print(pressed_key)
-        self.cursor_pos += 1
-        text_field.relocate_figure(self.cursor_obj, self.cursor_pos * self.char_spacing, 30)
-
-    def get_current_line(self):
-        pos = 0
-        for nline, line in enumerate(self.lines):
-            pos += len(line)
-            if pos >= self.pos:
-                return nline
-
-    def get_next_line_start(self):
-        pos = 0
-        for nline, line in enumerate(self.lines):
-            pos += len(line)
-            if pos >= self.pos:
-                return pos + 1
-
-    def get_perc(self):
-        return (self.get_current_line() - self.lines_above) / len(self.lines)
 
 
 def main():
@@ -96,7 +87,8 @@ def main():
 
     win = sg.Window('type trainer', layout, return_keyboard_events=True, use_default_focus=False)
 
-    text_field = win['-TXT-']  # type:Graph
+    graph = win['-TXT-']  # type:Graph
+
     line_nmb = 0
     while True:
         event, values = win.read()
@@ -104,21 +96,24 @@ def main():
             break
         elif event == '-GET-':
             url = values['-URL-']
-            content = Content(canvas_size=canvas_size)
+            content = Content(graph, canvas_size=canvas_size)
             content.from_url(url, line_length)
-            content.print_text(text_field, line_nmb=line_nmb)
+            content.print_text()
 
         elif event == 'Escape:27':
-            content.key_press(text_field, pressed_key=None)
+            print('escape')
+
+        elif event == 'Right:39':
+            content.move_cursor(1, 0)
+
+        elif event == 'Left:37':
+            content.move_cursor(-1, 0)
 
         elif event == 'Down:40':
-            line_nmb += 1
-            content.print_text(text_field, line_nmb=line_nmb)
+            content.move_cursor(0, 1)
 
         elif event == 'Up:38':
-            line_nmb -= 1
-            content.print_text(text_field, line_nmb=line_nmb)
-
+            content.move_cursor(0, -1)
 
         elif len(event) == 1:   # key strokes
             pressed_key = event
