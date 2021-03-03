9c4df596b1acdeb816a21b7000bef5d0cc96824c
torhen <torhen@gmail.com>
2021-03-03 14:37:46 +0100

    start of Graph version

--- psg_typing_trainer.py
+++ psg_typing_trainer.py
@@ -6,13 +6,16 @@ import textwrap
 
 class Content:
 
-    def __init__(self):
+    def __init__(self, canvas_size):
         self.lines = []
         self.pos = 0
-        self.font_size = 12
-        self.green = '#00AA00'
-        self.red = '#FF0000'
-        self.lines_above = 7 # how many lines visible above current
+        self.font = ('Courier', 14)
+        self.line_spacing = 30
+        self.char_spacing = 11
+        self.canvas_size = canvas_size
+        self.show_lines = int(1.5 * canvas_size[1] / self.line_spacing)
+        self.green = "#00AA00"
+        self.cursor_pos = 0
 
     def get_text(self):
         return '\n'.join(self.lines)
@@ -29,7 +32,6 @@ class Content:
         self.lines = lines
         self.pos = 0
 
-
     def change_pos(self, delta):
         new_pos = self.pos + delta
         if new_pos <=0:
@@ -43,30 +45,24 @@ class Content:
             pos = pos + len(self.lines[i]) + 1
         self.pos = pos
 
+    def print_text(self, text_field:sg.Graph, line_nmb):
+        pos = 0
+        text_field.draw_rectangle(top_left=(0, 0), bottom_right=(30, 30), fill_color=self.green)
+        lines = self.lines[line_nmb: line_nmb + self.show_lines]
+        text_field.erase()
+        for i, line in enumerate(lines):
+            text_field.draw_text(line, location=(0, pos),
+                    text_location=sg.TEXT_LOCATION_TOP_LEFT, font = self.font)
+            pos = pos + self.line_spacing
 
-    def print_text(self, text_field, cursor_color = None):
-        if cursor_color is None:
-            cursor_color = self.green
-        txt_content = self.get_text()
-        text_field.update('')
-        text_field.print(txt_content[:self.pos], end='', text_color='black')
-        text_field.print(txt_content[self.pos], end='', text_color='white', background_color=cursor_color)
-        text_field.print(txt_content[self.pos + 1:], end='', text_color='black')
-        text_field.set_focus()
-        text_field.set_vscroll_position(self.get_perc())
+        self.cursor_obj = text_field.draw_rectangle(top_left=(self.cursor_pos, 30),
+                                  bottom_right=(self.cursor_pos + self.char_spacing, 30),
+                                  line_color=self.green, line_width=5)
 
     def key_press(self, text_field, pressed_key):
-        txt_content = self.get_text()
-
-        cursor_letter = txt_content[self.pos]
-
-        if pressed_key == cursor_letter or pressed_key is None:  # escape
-            cursor_color = self.green
-            self.change_pos(1)
-        else:
-            cursor_color = self.red
-
-        self.print_text(text_field, cursor_color)
+        print(pressed_key)
+        self.cursor_pos += 1
+        text_field.relocate_figure(self.cursor_obj, self.cursor_pos * self.char_spacing, 30)
 
     def get_current_line(self):
         pos = 0
@@ -87,46 +83,49 @@ class Content:
 
 
 def main():
-    line_length = 100
+    line_length = 70
     font_size = 14
-    txtfield_size = (100, 30)
+    canvas_size = (800, 500)
 
     layout = [[sg.Input(key='-URL-', size=(100,),default_text='http://www.spiegel.de'), sg.Button('get', key='-GET-')],
-              [sg.Button('down', key='-DOWN-'), sg.Button('up', key='-UP-')],
-              [sg.Multiline(size=txtfield_size, key='-TXT-', font=('', font_size), disabled=True,enable_events=True)]
+              [sg.Graph(canvas_size=canvas_size, key='-TXT-',
+                        graph_bottom_left=(0, canvas_size[0]), graph_top_right=(canvas_size[0], 0),
+                        background_color='white',
+                        enable_events=True)]
               ]
 
     win = sg.Window('type trainer', layout, return_keyboard_events=True, use_default_focus=False)
 
-    text_field = win['-TXT-']  # type:sg.Multiline
-    test_line = 0
+    text_field = win['-TXT-']  # type:Graph
+    line_nmb = 0
     while True:
         event, values = win.read()
         if event is None:
             break
         elif event == '-GET-':
             url = values['-URL-']
-            content = Content()
+            content = Content(canvas_size=canvas_size)
             content.from_url(url, line_length)
-            content.print_text(text_field)
+            content.print_text(text_field, line_nmb=line_nmb)
 
         elif event == 'Escape:27':
             content.key_press(text_field, pressed_key=None)
 
-        elif event == '-DOWN-':
-            cur_line = content.get_current_line()
-            content.goto_line(cur_line + 1)
-            content.print_text(text_field)
+        elif event == 'Down:40':
+            line_nmb += 1
+            content.print_text(text_field, line_nmb=line_nmb)
 
-        elif event == '-UP-':
-            cur_line = content.get_current_line()
-            content.goto_line(cur_line - 1)
-            content.print_text(text_field)
+        elif event == 'Up:38':
+            line_nmb -= 1
+            content.print_text(text_field, line_nmb=line_nmb)
 
 
         elif len(event) == 1:   # key strokes
             pressed_key = event
             content.key_press(text_field, pressed_key=pressed_key)
+
+        else:
+            print(event)
 
 
 
