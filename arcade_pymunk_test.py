ef39a6d625115ed57f16fb8af6394936d3acf269
torhen <torhen@gmail.com>
2021-03-17 21:56:01 +0100

    Add files via upload

--- arcade_pymunk_test.py
+++ arcade_pymunk_test.py
@@ -7,7 +7,7 @@ space.gravity = 0, -1000
 
 class Ball:
     def __init__(self, space, x, y):
-        self.body = pymunk.Body(1, 1666)
+        self.body = pymunk.Body(1, 1000)
         self.body.position = x, y
 
         self.shape = pymunk.Circle(self.body, 20)
@@ -17,11 +17,10 @@ class Ball:
     def draw(self):
         x, y = self.body.position
         r = self.shape.radius
-        arcade.draw_circle_filled(x, y, r, color=arcade.color.RED)
+        arcade.draw_circle_outline(x, y, r, color=arcade.color.WHITE)
 
     def set_pos(self, x, y):
         self.body.position = x, y
-
 
 
 class Plane:
@@ -33,29 +32,28 @@ class Plane:
         space.add(self.body, self.shape)
 
     def draw(self):
-        arcade.draw_line(0, 100, 800, 50, color=arcade.color.BLUE)
-
-
-
+        arcade.draw_line(self.x0, self.y0, self.x1, self.y1, color=arcade.color.WHITE)
 
 
 class Game(arcade.Window):
     def __init__(self):
         super().__init__()
-        arcade.set_background_color(arcade.color.LIGHT_GRAY)
+        arcade.set_background_color(arcade.color.BLACK)
 
         self.ball = Ball(space, 50, 600)
-        self.plane = Plane(space, 0, 100, 800, 50)
+        self.plane1 = Plane(space, 0, 100, 800, 50)
+        self.plane2 = Plane(space, 0, 50, 800, 85)
 
     def on_draw(self):
         arcade.start_render()
         self.ball.draw()
-        self.plane.draw()
+        self.plane1.draw()
+        self.plane2.draw()
 
     def on_update(self, delta_time):
         space.step(delta_time)
         x, y = self.ball.body.position
-        if x > 1000:
+        if x > 800:
             self.ball.body.position = 50, 600
             self.ball.body.velocity = 0, 0
 
