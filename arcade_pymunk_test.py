a1b36c171fea5a7aa9043f505da90b37330a648e
torhen <torhen@gmail.com>
2021-03-17 20:53:10 +0100

    classes

--- arcade_pymunk_test.py
+++ arcade_pymunk_test.py
@@ -1,39 +1,64 @@
 import arcade
 import pymunk
 
-space = pymunk.Space()      # Create a Space which contain the simulation
-space.gravity = 0, -1000     # Set its gravity
+space = pymunk.Space()
+space.gravity = 0, -1000
 
-body = pymunk.Body(1, 1666)  # Create a Body with mass and moment
-body.position = 100, 500      # Set the position of the body
 
-shape = pymunk.Circle(body, 20) # Create a box shape and attach to body
-shape.elasticity = 0.8
-space.add(body, shape)       # Add both body and shape to the simulation
+class Ball:
+    def __init__(self, space, x, y):
+        self.body = pymunk.Body(1, 1666)
+        self.body.position = x, y
 
-segment_body = pymunk.Body(body_type=pymunk.Body.STATIC)
-segment_shape = pymunk.Segment(segment_body, (0, 100), (800, 50), 5)
-segment_shape.elasticity = 0.8
-space.add(segment_body, segment_shape)
+        self.shape = pymunk.Circle(self.body, 20)
+        self.shape.elasticity = 0.83
+        space.add(self.body, self.shape)
+
+    def draw(self):
+        x, y = self.body.position
+        r = self.shape.radius
+        arcade.draw_circle_filled(x, y, r, color=arcade.color.RED)
+
+    def set_pos(self, x, y):
+        self.body.position = x, y
+
+
+
+class Plane:
+    def __init__(self, space, x0, y0, x1, y1):
+        self.x0, self.y0, self.x1, self.y1 = x0, y0, x1, y1
+        self.body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        self.shape = pymunk.Segment(self.body, (x0, y0), (x1, y1), 5)
+        self.shape.elasticity = 0.83
+        space.add(self.body, self.shape)
+
+    def draw(self):
+        arcade.draw_line(0, 100, 800, 50, color=arcade.color.BLUE)
+
+
+
 
 
 class Game(arcade.Window):
     def __init__(self):
         super().__init__()
         arcade.set_background_color(arcade.color.LIGHT_GRAY)
-        self.x = 0
-        self.y = 0
+
+        self.ball = Ball(space, 50, 600)
+        self.plane = Plane(space, 0, 100, 800, 50)
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_circle_filled(self.x, self.y, 20, color=arcade.color.RED)
-        arcade.draw_line(0, 100, 800, 50, color=arcade.color.BLUE)
+        self.ball.draw()
+        self.plane.draw()
 
     def on_update(self, delta_time):
         space.step(delta_time)
-        pos = body.position
-        self.x = pos.x
-        self.y = pos.y
+        x, y = self.ball.body.position
+        if x > 1000:
+            self.ball.body.position = 50, 600
+            self.ball.body.velocity = 0, 0
+
 
 
 game = Game()
